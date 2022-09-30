039dde394da6200b0dc056342ac5c76a87ce0290
Giancarmine Salucci <giancarmine@gmail.com>
2022-09-30 22:49:04 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# nginx-certbot-docker-compose
+
+## First run
+``` bash
+./first-start.sh
+```
+
+enter your hostname
+enter the email for letsencrypt
+
+on success
+
+## configure target container
+
+add your service to docker compose.
+
+Example:
+
+
+``` yaml
+
+services: 
+
+...
+    yourService:
+        image: yourImage
+
+```
+
+add your service to:  `./nginx/nginx.conf`
+
+
+``` conf
+...
+server {
+    listen 443 ssl;
+    ...
+    ssl_certificate_key ...;
+
+    location / {
+        proxy_pass http://yourService;
+    }
+}
+...
+
+```
---
+++ first-start.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+nginxConfig=$(pwd)/nginx/nginx.conf
+initLetsencryptScript=$(pwd)/init_letsencrypt.sh
+exampleHostName=example.org
+exampleContainerName=app_name
+
+sudo rm -rf ./certbot ./nginx
+
+mkdir ./certbot
+mkdir ./nginx
+cp ./initialConf/nginx.conf ./nginx/nginx.conf
+cp ./initialConf/init_letsencrypt.sh ./init_letsencrypt.sh
+read -p "Nome host:" hostName
+#read -p "Nome container da proxare: " containerName 
+read -p "email per rinnovo certificati: " emailAddr 
+
+#rimpiazzo tutte le occorrenze di example.org con $nomeHost nella configurazione di nginx
+sed -i "s/$exampleHostName/$hostName/g" $nginxConfig
+
+#rimpiazzo tutte le occorrenze di $exampleContainerName con $containerName nella configurazione di nginx
+#sed -i "s/$exampleContainerName/$containerName/g" $nginxConfig
+
+#rimpiazzo tutte le occorrenze di example.org con $nomeHost nello script iniziale di letsencrypt
+sed -i "s/$exampleHostName/$hostName/g" $initLetsencryptScript
+
+#setto email di rinnovo nello script di letsencrypt
+sed -i "s/email=.*/email=$emailAddr/" $initLetsencryptScript
+
+chmod +x $initLetsencryptScript
+echo "Avvio script letsencrypt e compose"
+sudo $initLetsencryptScript && docker-compose up
+
